0d9ca344e489e4d47275b1e245708e033d3751e7
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-04-22 17:10:47 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container-fluid">
+        <PageHeader></PageHeader>
+
+    </div>
+</template>
+
+<script>
+// IMPORTAZIONE COMPONENTI
+import PageHeader from "./components/PageHeader.vue";
+export default {
+    components: {
+        PageHeader
+    }
+}
+</script>
+<style lang="scss">
+@import "bootstrap/scss/bootstrap";
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="container">
+        <div class="row justify-content-between">
+            <div class="col-6">LOCATION</div>
+            <div class="col-6 text-end">LINKS</div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
